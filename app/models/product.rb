f33794867e4a7b00d268b8c36b06468b49ffcba8
Philip <oumaphilip01@gmail.com>
2025-04-10 17:15:25 +0300

    update product model to use active storage

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,17 +1,16 @@
 class Product < ApplicationRecord
+  belongs_to :user # Remove `optional: true` if every product must belong to a user
 
-  # before_destroy :not_referenced_by_any_line_item
-  belongs_to :user, optional: true
-
-  mount_uploader :image, ImageUploader
-  serialize :image, JSON # If you use SQLite, add this line
+  # If using Active Storage instead of CarrierWave
+  has_one_attached :image
 
   validates :title, :brand, :price, :model, presence: true
   validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed. " }
   validates :title, length: { maximum: 140, too_long: "%{count} characters is the maximum allowed. " }
   validates :price, length: { maximum: 10 }
+  validates :image, presence: true
 
-  # You can input more brands finish and condition here
+  # Constants for brands, finishes, and conditions
   BRAND = %w{ Ferrari Opel Lenovo Fossil }
   FINISH = %w{ Black White Navy Blue Red Clear Satin Yellow Seafoam }
   CONDITION = %w{ New Excellent Mint Used Fair Poor }

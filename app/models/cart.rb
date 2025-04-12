5371ff736e37157d512a0090ba5fad5e014ab663
Philip <oumaphilip01@gmail.com>
2025-04-12 08:56:22 +0300

    feat: initialize cart implementation. add cart model

---
+++ app/models/cart.rb
@@ -0,0 +1,24 @@
+# app/models/cart.rb
+class Cart < ApplicationRecord
+    has_many :line_items, dependent: :destroy
+    
+    def add_product(product)
+      line_item = line_items.find_by(product_id: product.id)
+      
+      if line_item
+        line_item.quantity += 1
+      else
+        line_item = line_items.build(product_id: product.id)
+      end
+      
+      line_item
+    end
+    
+    def total_price
+      line_items.sum { |item| item.total_price }
+    end
+    
+    def total_items
+      line_items.sum(:quantity)
+    end
+  end

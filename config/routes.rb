5f76ae46b1c661fee0b824339e023a82c11ba641
Philip <oumaphilip01@gmail.com>
2025-04-12 09:15:27 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,16 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
+
+  resources :carts, only: [:show, :destroy]
+  
+  resources :line_items, only: [:create, :destroy] do
+    member do
+      patch :increase
+      patch :decrease
+    end
+  end
+  
   root 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'upload', to: 'uploads#create'

eec855da2d5a4cb5aa8d763e7a2f540c2297b951
Philip <oumaphilip01@gmail.com>
2025-04-13 23:50:11 +0300

    fix: user registration to pick user's name

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,18 @@
 # This must be implemented.
 # sign_up_params and account_update_params
 class RegistrationsController < Devise::RegistrationsController
-
- private
- 
-end
+    # This is a key area to check - strong parameters must include :name
+    def sign_up_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def account_update_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+    end
+    
+    # For debugging
+    def create
+      puts "SIGNUP PARAMS: #{sign_up_params.inspect}" # This will appear in your server logs
+      super
+    end
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_many :products, dependent: :destroy
+  has_many :products, dependent: :destroy
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
 end

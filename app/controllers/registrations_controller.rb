8067d0816eea2c142a181677efd7234329a5f7bf
Philip <oumaphilip01@gmail.com>
2025-04-14 00:14:12 +0300

    add cart icons

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,4 @@
-# This must be implemented.
-# sign_up_params and account_update_params
 class RegistrationsController < Devise::RegistrationsController
-    # This is a key area to check - strong parameters must include :name
     def sign_up_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
@@ -10,9 +7,9 @@ class RegistrationsController < Devise::RegistrationsController
       params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
     end
     
-    # For debugging
-    def create
-      puts "SIGNUP PARAMS: #{sign_up_params.inspect}" # This will appear in your server logs
-      super
-    end
+    # # For debugging
+    # def create
+    #   puts "SIGNUP PARAMS: #{sign_up_params.inspect}" # This will appear in your server logs
+    #   super
+    # end
   end

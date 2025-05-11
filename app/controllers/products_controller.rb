3ad024c35c6e0b4e60f88832da66b071f9905803
hokwach <hezronokwach@gmail.com>
2025-05-11 13:49:57 +0300

    fix: update redirect path in destroy action for consistency

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -59,7 +59,7 @@ class ProductsController < ApplicationController
   def destroy
     @product.destroy
     respond_to do |format|
-      format.html { redirect_to products_url, notice: "Product was successfully deleted." }
+      format.html { redirect_to products_path, notice: "Product was successfully deleted." }
       format.json { head :no_content }
     end
   end
